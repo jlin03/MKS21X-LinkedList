1feac13e1a69eb2e875aeeca2e8d059d84b7e45f
jlin03 <jlin03@stuy.edu>
2018-12-13 09:59:24 -0500

    empty constructor and set methods written

--- Node.java
+++ Node.java
@@ -1,27 +1,33 @@
 public class Node{
-	private int data;
+	private Integer data;
 	private Node next,prev;
-	
-	public Node(int val) {
+
+	public Node() {}
+
+	public Node(Integer val) {
 		data = val;
 	}
-	
+
 	public int get() {
 		return data;
 	}
-	
+
+	public set(Integer val) {
+		data = val;
+	}
+
 	public Node next() {
 		return next;
 	}
- 
+
 	public Node prev() {
 		return prev;
 	}
-	
+
 	public void setNext(Node n) {
 		next = n;
 	}
-	
+
 	public void setPrev(Node p) {
 		prev = p;
 	}
