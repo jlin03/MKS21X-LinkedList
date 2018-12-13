53282d1e2a3cc043516dfe05b800da8ec848b7be
jlin03 <jlin03@stuy.edu>
2018-12-12 20:19:37 -0500

    node class coded with methods next, prev, setnext, setprev, constructor

---
+++ Node.java
@@ -0,0 +1,26 @@
+public class Node{
+	private int data;
+	private Node next,prev;
+	
+	public Node(int val, Node n, Node p) {
+		data = val;
+		next = n;
+		prev = p;
+	}
+	
+	public Node next() {
+		return next;
+	}
+ 
+	public Node prev() {
+		return prev;
+	}
+	
+	public void setNext(Node n) {
+		next = n;
+	}
+	
+	public void setPrev(Node p) {
+		prev = p;
+	}
+}
